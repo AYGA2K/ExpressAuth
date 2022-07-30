5ea9d34f7b56f3cb8f48a3d6a024a216ad4f23b5
AYGA2K <garouatayoub@gmail.com>
2022-07-30 11:00:33 +0000

    up

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+const posts = [
+  {
+    username: "ayoub",
+    title: "post1",
+  },
+  {
+    username: "bilal",
+    title: "post2",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
 });
+app.get("/login", (req, res) => {
+  // Authenticate User
+  const username = req.body.username;
 
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
